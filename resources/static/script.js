b6c88260e6df2e5dd19e46b6b19fec8f7d021571
ScottyP666 <Scott_M_G@live.com>
2022-05-22 23:09:22 -0700

    troubleshooting
    Trying to get the theme switcher to work. *this* within the for loop no longer references what its supposed too

--- resources/static/script.js
+++ resources/static/script.js
@@ -1,41 +1,82 @@
-let themeDots = document.getElementsByClassName('theme-dot');
+let themeDots = document.getElementsByClassName('theme-dot')
 
 let theme = localStorage.getItem('theme')
 
-if (theme == null) {
+if (theme === null) {
     setTheme('light')
 }else{
     setTheme(theme)
 }
 
+
+
 for (var i = 0; themeDots.length > i; i++) {
-    themeDots[i].addEventListener('click', function(){
-        let mode = this.dataset.mode;
-        console.log('Option clicked: ', mode);
-        setTheme(mode);
+    themeDots[i].addEventListener('click', () => {
+        let mode = this.dataset.mode
+        console.log('Option clicked: ', mode)
+        setTheme(mode)
     })
 };
 
+
 function setTheme(mode) {
     if(mode == 'light'){
-        document.getElementById('theme-style').href = '/static/default.css'
+        document.querySelector('.theme-dot').href = 'default.css';
     }
 
     if(mode == 'blue'){
-        document.getElementById('theme-style').href = '/static/blue.css'
+        document.querySelector('.theme-dot').href = 'resources/static/blue.css';
     }
 
     if(mode == 'green'){
-        document.getElementById('theme-style').href = '/static/green.css'
+        document.querySelector('#green-mode').href = '/resources/static/green.css';
     }
 
     if(mode == 'purple'){
-        document.getElementById('theme-style').href = '/static/purple.css'
+        document.querySelector('.theme-dot').href = 'resources/static/purple.css';
     }
 
     if(mode == 'blasphemy'){
-        document.getElementById('theme-style').href = '/static/Satan.css'
+        document.querySelector('.theme-dot').href = 'resources/static/Satan.css';
     }
 
     localStorage.setItem('theme', mode)
 }
+
+
+
+//refactoring the above code for new JS knowledge
+
+/*
+
+
+
+
+
+//this is a refactor of the above code to use a switch case instead
+
+/*
+function setTheme(mode) {
+    if(mode == 'light'){ 
+        document.getElementById('theme-style').href = '/resources/static/default.css'
+    }
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'static/blue.css'
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = '/resources/static/green.css'
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    if(mode == 'blasphemy'){
+        document.getElementById('theme-style').href = 'Satan.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
+*/
