37e9cd54c1762a992865267c8ccf567fc9196c19
Scott Garcia <63470831+ScottyG666@users.noreply.github.com>
2022-09-20 18:23:15 -0700

    organization and theme swap fix

---
+++ resources/static/js/script.js
@@ -0,0 +1,46 @@
+let themeDots = document.getElementsByClassName('theme-dot')
+
+let theme = localStorage.getItem('theme')
+
+if (theme === null) {
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+for (var i = 0; themeDots.length > i; i++) {
+    console.log(themeDots[i]);
+    };
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode;
+        console.log('Option clicked: ', mode);
+        setTheme(mode);
+    })
+};
+
+
+function setTheme(mode) {
+    if(mode == 'light'){
+        document.querySelector('#theme-style').href = 'resources/static/css/default.css';
+    }
+
+    if(mode == 'blue'){
+        document.querySelector('#theme-style').href = 'resources/static/css/blue.css';
+    }
+
+    if(mode == 'green'){
+        document.querySelector('#theme-style').href = 'resources/static/css/green.css';
+    }
+
+    if(mode == 'purple'){
+        document.querySelector('#theme-style').href = 'resources/static/css/purple.css';
+    }
+
+    if(mode == 'blasphemy'){
+        document.querySelector('#theme-style').href = 'resources/static/css/Satan.css';
+    }
+
+    localStorage.setItem('theme', mode)
+}
